8da1367b9be8356f49dccdf23f27e2eeb76dc151
Cooper Hanke <cooper.hanke@gmail.com>
2019-11-24 09:46:11 -0600

    Add project files.

---
+++ BlazorDemo/Server/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using BlazorDemo.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDemo.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<PersonModel> GetPeople()
+        {
+            var output = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "Tim", LastName = "Corey", AccountBalance = 19.45M },
+                new PersonModel { FirstName = "Mary", LastName = "Lou", AccountBalance = 105.87M },
+                new PersonModel { FirstName = "John", LastName = "Smith", AccountBalance = 115.32M }
+            };
+            return output;
+        }
+
+        [HttpPost]
+        public void Post(PersonModel person)
+        {
+
+        }
+    }
+}
